1766f26504331e272c61f851b3d895b20550e1a4
zedmatrix <zedmatrix007@gmail.com>
2024-10-18 13:30:11 -0700

    Update Start_qemu.sh

--- Start_qemu.sh
+++ Start_qemu.sh
@@ -1,36 +1,37 @@
+#!/bin/bash
+# A Simple Bash Script to Start a qemu vm
 System="-M pc-q35-8.2 -enable-kvm -m 8G -smp 4"
-
+# this is copied from OVMF.fd and renamed to LFS_UEFI.flash to boot UEFI Mode
 UEFI="-drive if=pflash,format=raw,file=LFS_UEFI.flash"
+
+# create like> qemu-img create -f qcow2 lfs_12_uefi.img 50G
 LFS="-drive id=disk0,if=none,format=qcow2,file=lfs_12_uefi.img -device virtio-blk-pci,drive=disk0,bootindex=0"
-LFSsource="-drive id=disk1,if=none,format=qcow2,file=lfs-source.img -device virtio-blk-pci,drive=disk1,bootindex=1"
-XLFS="-drive id=disk2,if=none,format=qcow2,file=XLFS_12-2.img -device virtio-blk-pci,drive=disk2,bootindex=2"
+# a scratch drive created in a similar way and you can restrict to smaller size like 3G
+Source="-drive id=disk1,if=none,format=qcow2,file=lfs-source.img -device virtio-blk-pci,drive=disk1,bootindex=1"
 
-GenRoot="-drive id=disk1,if=none,format=qcow2,file=Gen_Root.img -device virtio-blk-pci,drive=disk1,bootindex=1"
+# LFS Source CD Rom created from the host> mkisofs -o lfs-source.iso -R -J lfs_files/
+LFSsource="-drive file=lfs-source.iso,media=cdrom"
+# BLFS Source CD Rom created from the host> mkisofs -o blfs-source.iso -R -J blfs_files/
+BLFSsource="-drive file=blfs-source.iso,media=cdrom"
+
+# the Gentoo Live CD and Writable Drive like a usb 
 GENTOO="-drive id=cd0,if=none,format=raw,readonly=on,file=Live_Gentoo_Min.iso -device ide-cd,bus=ide.1,drive=cd0,bootindex=2"
+# created for downloading the stage 3 tar and extracting to 
+# qemu-img create -f qcow2 Gen_Root.img 3G
+GenRoot="-drive id=disk1,if=none,format=qcow2,file=Gen_Root.img -device virtio-blk-pci,drive=disk1,bootindex=1"
 
-#LFS Source CD Rom
-#Source="-drive file=cd-source.iso,media=cdrom"
-
-#BLFS Source CD Rom
-Source="-drive file=blfs-source.iso,media=cdrom"
-
-#Mount USB drive
-USB="-drive file=/dev/sdf,media=disk"
-
-#Suspend="-global PIIX4_PM.disable_s3=0"
+# some global options to start the qemu VM
 Logfile="-global isa-debugcon.iobase=0x402 -debugcon file:gentoo.ovmf.log"
 Monitor="-monitor stdio"
 Serial="-serial mon:stdio"
 Mouse="-device piix3-usb-uhci -device usb-tablet"
 Network="-netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,romfile="
-
 Video="-device qxl-vga,xres=1440,yres=900"
-
-#Video="-vga qxl -display gtk,gl=on"
-
+# Still a work in progress and experimenting with spice
 Spice="-spice port=5900,addr=127.0.0.1,disable-ticketing=on -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent"
-
-#Spice="-spice port=5900,disable-ticketing=on"
-#Clipboard="-device virtio-serial-pci -chardev spicevmc,id=vdagent,debug=0,name=vdagent -device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-
-qemu-system-x86_64 $System $UEFI $LFS $LFSsource $Logfile $Monitor $Mouse $Network $Video
+# Mount USB drive ... for me this required sudo 
+USB="-drive file=/dev/sdf,media=disk"
+# This starts the qemu vm with the options you select 
+# Needed: $System $UEFI $Logfile $Monitor $Mouse $Network $Video 
+# Plus: ONE Bootable Drive
+qemu-system-x86_64 $System $UEFI $LFS $Source $GENTOO $GenRoot $Logfile $Monitor $Mouse $Network $Video
