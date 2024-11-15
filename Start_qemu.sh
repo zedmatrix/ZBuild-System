f934adce83995de433a24ad683fdeccd4ff54b5b
zedmatrix <zedmatrix007@gmail.com>
2024-11-15 08:01:40 -0800

    Filesystem Rename and slight move

--- Start_qemu.sh
+++ Start_qemu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # A Simple Bash Script to Start a qemu vm
 System="-M pc-q35-8.2 -enable-kvm -m 8G -smp 4"
+
 # this is copied from OVMF.fd and renamed to LFS_UEFI.flash to boot UEFI Mode
 UEFI="-drive if=pflash,format=raw,file=LFS_UEFI.flash"
 
-# create like> qemu-img create -f qcow2 lfs_12_uefi.img 50G
+# create like image $ qemu-img create -f qcow2 lfs_12_uefi.img 50G
 LFS="-drive id=disk0,if=none,format=qcow2,file=lfs_12_uefi.img -device virtio-blk-pci,drive=disk0,bootindex=0"
 # a scratch drive created in a similar way and you can restrict to smaller size like 3G
 Source="-drive id=disk1,if=none,format=qcow2,file=lfs-source.img -device virtio-blk-pci,drive=disk1,bootindex=1"
 
-# LFS Source CD Rom created from the host> mkisofs -o lfs-source.iso -R -J lfs_files/
+# LFS Source CD Rom created from the host $ mkisofs -o lfs-source.iso -R -J lfs_files/
 LFSsource="-drive file=lfs-source.iso,media=cdrom"
-# BLFS Source CD Rom created from the host> mkisofs -o blfs-source.iso -R -J blfs_files/
+# BLFS Source CD Rom created from the host $ mkisofs -o blfs-source.iso -R -J blfs_files/
 BLFSsource="-drive file=blfs-source.iso,media=cdrom"
 
 # the Gentoo Live CD and Writable Drive like a usb 
@@ -21,14 +22,16 @@ GENTOO="-drive id=cd0,if=none,format=raw,readonly=on,file=Live_Gentoo_Min.iso -d
 GenRoot="-drive id=disk1,if=none,format=qcow2,file=Gen_Root.img -device virtio-blk-pci,drive=disk1,bootindex=1"
 
 # some global options to start the qemu VM
-Logfile="-global isa-debugcon.iobase=0x402 -debugcon file:gentoo.ovmf.log"
+Logfile="-global isa-debugcon.iobase=0x402 -debugcon file:uefi-boot.log"
+# Setups up the terminal for command usage
 Monitor="-monitor stdio"
-Serial="-serial mon:stdio"
+
 Mouse="-device piix3-usb-uhci -device usb-tablet"
+# Setup network to listen on 2222 and redirect to vm 22 for ssh
 Network="-netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,romfile="
+# Video resolution for the uefi console
 Video="-device qxl-vga,xres=1440,yres=900"
-# Still a work in progress and experimenting with spice
-Spice="-spice port=5900,addr=127.0.0.1,disable-ticketing=on -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent"
+
 # Mount USB drive ... for me this required sudo 
 USB="-drive file=/dev/sdf,media=disk"
 # This starts the qemu vm with the options you select 
--- chapter_2/README.md
+++ chapter_2/README.md
@@ -1,3 +1,5 @@
 # <p align="center">Chapter 2</p>
 ## Build Helper Scripts
 ### For Use With Zbuild Script System
+version 2 updates
+
