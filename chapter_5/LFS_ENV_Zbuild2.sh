d400570e1101e1aa3771a25fc56cab6fae6bc2d1
zedmatrix <zedmatrix007@gmail.com>
2024-11-15 09:55:31 -0800

    Binutils chapter 5,lfs zbuild2

---
+++ chapter_5/LFS_ENV_Zbuild2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+RED="\e[1;31m"
+GREEN="\e[1;32"
+YELLOW="\e[1;33m"
+NORMAL="\e[0m"
+
+LFS=/mnt/lfs
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+BUILD_ROOT="/BUILD"
+BUILD_SOURCE="/opt/source"
+BUILD_LOG="${BUILD_ROOT}/Zbuild_Logs"
+BUILD_CMD="${BUILD_ROOT}/zbuild2.sh"
+
+Src_Extract() {
+	print "Extracting: ${archive}"
+	mkdir -v "${BUILD_ROOT}/${packagedir}"
+	tar -xf "${BUILD_SOURCE}/${archive}" -C "${BUILD_ROOT}/${packagedir}" --strip-components=1
+}
+
+print() { printf "${RED}*** ${YELLOW}%s ${RED}***${NORMAL}\n" "$*"; }
+
+
+export RED GREEN YELLOW NORMAL
+export BUILD_CMD BUILD_LOG BUILD_ROOT BUILD_SOURCE LFS LFS_TGT
+export -f Src_Extract print
