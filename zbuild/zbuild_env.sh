98b9debb2e0834abdd7cf44d521061564b886c3f
zedmatrix <zedmatrix007@gmail.com>
2024-11-14 19:02:09 -0800

    Update zbuild_env.sh

--- zbuild/zbuild_env.sh
+++ zbuild/zbuild_env.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 # begin /etc/profile.d/zbuild.sh
+unset pkg pkgver package packagedir delete patch
+unset prefix build sysconfdir statedir disable enable
+
+RED="\e[1;31m"
+GREEN="\e[1;32"
+YELLOW="\e[1;33m"
+NORMAL="\e[0m"
+
+BUILD_ROOT="/BUILD"
+BUILD_SOURCE="/opt/source"
+BUILD_LOG="${BUILD_ROOT}/Zbuild_Logs"
+BUILD_CMD="${BUILD_ROOT}/zbuild2.sh"
+
 PackageCheck() {
     pkg=${1}
     [ -z $pkg ] && { echo "Usage: PackageCheck 'Package Name' "; exit 1; }
@@ -19,20 +32,18 @@ Src_Extract() {
     tar -xf "${BUILD_SOURCE}/${archive}" -C "${BUILD_ROOT}/${packagedir}" --strip-components=1
 }
 
-print() { printf "*** %s ***\n" "$*"; }
+print() { printf "${YELLOW} *** %s *** ${NORMAL} \n" "$*"; }
 
-RED="\e[1;31m"
-GREEN="\e[1;32"
-YELLOW="\e[1;33m"
-NORMAL="\e[0m"
-
-BUILD_ROOT="/BUILD"
-BUILD_SOURCE="/opt/source"
-BUILD_LOG="${BUILD_ROOT}/Zbuild_Logs"
-BUILD_CMD="${BUILD_ROOT}/zbuild2.sh"
+SRCGET() {
+        if [ -z $1 ]; then
+                echo "Atleast one argument is needed"
+        else
+                wget $1 --no-clobber --rejected-log=${BUILD_LOG}/wget_rejects.log -P ${BUILD_SOURCE}
+        fi
+}
 
 export RED GREEN YELLOW NORMAL
 export BUILD_CMD BUILD_LOG BUILD_ROOT BUILD_SOURCE
-export -f PackageCheck print Src_Extract
+export -f PackageCheck Src_Extract print SRCGET
 
 # end /etc/profile.d/zbuild.sh
