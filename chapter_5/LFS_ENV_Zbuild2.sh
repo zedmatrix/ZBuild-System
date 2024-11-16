5cf4db7fc622b2a10c08c81877676f6a3d345ed5
zedmatrix <zedmatrix007@gmail.com>
2024-11-16 11:15:57 -0800

    Update LFS_ENV_Zbuild2.sh

--- chapter_5/LFS_ENV_Zbuild2.sh
+++ chapter_5/LFS_ENV_Zbuild2.sh
@@ -6,19 +6,20 @@ NORMAL="\e[0m"
 
 LFS=/mnt/lfs
 LFS_TGT=$(uname -m)-lfs-linux-gnu
-BUILD_ROOT="${LFS}/BUILD"
-BUILD_SOURCE="${LFS}/opt/source"
-BUILD_LOG="${BUILD_ROOT}/Zbuild_Logs"
-BUILD_CMD="${BUILD_ROOT}/zbuild2.sh"
+# Possible breakage in the yasm/nasm building of libvpx-1.15.0
+ZBUILD_root="${LFS}/BUILD"
+ZBUILD_sources="${LFS}/opt/source"
+ZBUILD_log="${ZBUILD_root}/Zbuild_Log"
+ZBUILD_script="${ZBUILD_root}/zbuild2.sh"
 
 Src_Extract() {
 	print "Extracting: ${archive}"
-	mkdir -v "${BUILD_ROOT}/${packagedir}"
-	tar -xf "${BUILD_SOURCE}/${archive}" -C "${BUILD_ROOT}/${packagedir}" --strip-components=1
+	mkdir -v "${ZBUILD_root}/${packagedir}"
+	tar -xf "${ZBUILD_sources}/${archive}" -C "${ZBUILD_root}/${packagedir}" --strip-components=1
 }
 
 print() { printf "${YELLOW}*** %s ***${NORMAL}\n" "$*"; }
 
 export RED GREEN YELLOW NORMAL
-export BUILD_CMD BUILD_LOG BUILD_ROOT BUILD_SOURCE LFS LFS_TGT
+export ZBUILD_root ZBUILD_sources ZBUILD_log ZBUILD_script LFS LFS_TGT
 export -f Src_Extract print
