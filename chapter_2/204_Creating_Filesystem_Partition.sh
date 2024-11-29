b91e4402944f0014eba5600bcac003a263e2c1e8
zedmatrix <zedmatrix007@gmail.com>
2024-11-29 12:52:20 -0800

    chapter 2/4 fix

---
+++ chapter_2/204_Creating_Filesystem_Partition.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Formatting/Creating Paritions"
+# zbuild 2.0 cross compile environment
+
+echo "Setting ROOT and SWAP Partitions"
+ROOT=sda3
+SWAP=sda2
+BOOT=sda1
+
+if blkid /dev/$ROOT | grep -q 'TYPE="ext4"'; then
+    echo "/dev/$ROOT is already formatted as ext4."
+else
+    echo "*** Formatting /dev/$ROOT as ext4"
+    mkfs.ext4 /dev/$ROOT
+fi
+
+if blkid /dev/$BOOT | grep -q 'TYPE="vfat"'; then
+    echo "/dev/$BOOT is already formatted as vfat."
+else
+    echo "*** Formatting /dev/$BOOT as vfat "
+    mkfs.vfat /dev/$ROOT
+fi
+
+if blkid /dev/$SWAP | grep -q 'TYPE="swap"'; then
+    echo "/dev/$SWAP is already formatted as swap."
+else
+    echo "*** Creating Swap space /dev/$SWAP "
+    mkswap /dev/$SWAP
+    swapon
+fi
+
+echo "* Done - Partitions Formatted *"
--- chapter_2/301_wget_source_files.sh
+++ chapter_2/301_wget_source_files.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+wget="${PWD}/301-wgetlist"
+md5="${PWD}/301-md5sums"
+[ ! -f $wget ] && { echo "Missing $wget"; exit 1; }
+[ ! -f $md5 ] && { echo "Missing $md5"; exit 1; }
+
 if [ -n "$LFS" ] && [ -d "$LFS" ]; then
    echo "* Good $LFS is set *"
    
    if [ -d "${ZBUILD_sources}" ] && [ -n "${ZBUILD_sources}" ]; then
       echo "* Good ${ZBUILD_sources} is set and exists *"
-   
       chmod -v a+wt "$ZBUILD_sources"
       
       pushd $ZBUILD_sources
-        if [ -f wget-list ] && [ -f md5sums ]; then
-          wget --input-file=wget-list --continue
-          md5sum -c md5sums
+          grep -v '^#' $wget | awk '{print $1}' | wget -i- -c
+          md5sum -c $md5 > "${ZBUILD_log}/301_wgetlist.log"
           chown root:root "${ZBUILD_sources}/*"
           echo "* Finished Getting Sources to: ${ZBUILD_sources} *"
-        else
-          echo "Error: make sure 'wget-list' and 'md5sums' are in ${ZBUILD_sources}"
-        fi
       popd
    else
       echo "Error: {ZBUILD_sources} is not available. *"
