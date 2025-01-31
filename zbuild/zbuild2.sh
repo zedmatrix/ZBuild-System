e5dfa24f397d547cd27ac6fccbc7050bf27f9741
zedmatrix <zedmatrix007@gmail.com>
2025-01-31 10:16:06 -0800

    Update zbuild2.sh

--- zbuild/zbuild2.sh
+++ zbuild/zbuild2.sh
@@ -3,7 +3,7 @@
 #   Source zbuild_env.sh or add it to /etc/profile.d
 #
 # REQUIRES: ZBUILD_root, ZBUILD_sources, ZBUILD_log, zprint function
-#
+# 	zzred, zzgreen, zzyellow, zznormal
 # To-Do: ZBUILD_user, ZBUILD_group
 #   rename patch and delete to prepend ZBUILD_
 #
@@ -11,75 +11,75 @@
 #
 #   Requires ${archive}, ${package}, ${packagedir}
 #
-fail() {
+zzfail() {
     local exitcode="${2:-1}"
     zprint " Exiting with code $exitcode: $1"
     exit $exitcode
 }
-bail() { zprint "${RED} Bailing: $* ${NORMAL}"; exit 1; }
-warn() { zprint "${YELLOW} Warning: $* ${NORMAL}"; }
-stars() { printf "${GREEN}"; printf '%.0s*' {1..30}; printf "${NORMAL}\n"; }
+zzbail() { zprint "${zzred} Bailing: $* ${zznormal}"; exit 1; }
+zzwarn() { zprint "${zzyellow} Warning: $* ${zznormal}"; }
+zzstars() { printf "${zzgreen}"; printf '%.0s*' {1..30}; printf "${zznormal}\n"; }
 
-[ -z "$archive" ] && bail "Archive not set"
-[ -z "$package" ] && bail "Package not set"
-[ -z "$packagedir" ] && bail "Packagedir not set"
+[ -z "$archive" ] && zzbail "Archive not set"
+[ -z "$package" ] && zzbail "Package not set"
+[ -z "$packagedir" ] && zzbail "Packagedir not set"
 
 #   verify the archive is in BUILD_SOURCE
-[ ! -e "$ZBUILD_sources/$archive" ] && bail "Missing Source Archive"
+[ ! -e "$ZBUILD_sources/$archive" ] && zzbail "Missing Source Archive"
 
 pushd $ZBUILD_root
-	stars
+    zzstars
     if declare -f Src_Extract > /dev/null; then
-        Src_Extract || bail "Extraction: $?"
+        Src_Extract || zzbail "Extraction: $?"
     else
         zprint "Skipping Extraction"
     fi
 
-    [ ! -d "$ZBUILD_root/$packagedir" ] && bail "$packagedir Failure $?"
+    [ ! -d "$ZBUILD_root/$packagedir" ] && zzbail "$packagedir Failure $?"
 
     pushd $ZBUILD_root/$packagedir
 
 # Run a $patch if set
         if [ ! "$patch" = "false" ] && [ -f "${ZBUILD_sources}/${patch}" ]; then
             zprint "Patching ${package} with ${patch}"
-            patch -Np1 -i "${ZBUILD_sources}/${patch}" || bail "Patching Failed $?"
+            patch -Np1 -i "${ZBUILD_sources}/${patch}" || zzbail "Patching Failed $?"
         else
             zprint "Skipping Patch"
         fi
 
 # Src_prepare function (sed, mkdir/cd)
-        type -t Src_prepare &>/dev/null && Src_prepare || warn "Src_prepare not set."
+        type -t Src_prepare &>/dev/null && Src_prepare || zzwarn "Src_prepare not set."
 
 # Src_configure (configure/meson/cmake)
         if declare -f Src_configure > /dev/null; then
-            Src_configure || bail "Configure: $?"
+            Src_configure || zzbail "Configure: $?"
         else
-            fail "Src_configure not set. Exiting." 88
+            zzfail "Src_configure not set. Exiting." 88
         fi
 
 # Src_compile (make/ninja)
         if declare -f Src_compile > /dev/null; then
-            Src_compile || bail "Compile: $?"
+            Src_compile || zzbail "Compile: $?"
         else
-            fail "Src_compile not set. Exiting." 77
+            zzfail "Src_compile not set. Exiting." 77
         fi
 
 # Src_check (make/ninja)
         if declare -f Src_check > /dev/null; then
-            Src_check || warn "Warning: $?"
+            Src_check || zzwarn "Warning: $?"
         else
-            fail "Src_check not set. Exiting." 66
+            zzfail "Src_check not set. Exiting." 66
         fi
 
 # Src_install (make/ninja)
         if declare -f Src_install > /dev/null; then
-            Src_install || bail "Install Failure ${package}: $?"
+            Src_install || zzbail "Install Failure ${package}: $?"
         else
-            fail "Src_install not set. Exiting." 55
+            zzfail "Src_install not set. Exiting." 55
         fi
 
 # Src_post function (document intalls)
-        type -t Src_post &>/dev/null && Src_post || warn "Src_post not set."
+        type -t Src_post &>/dev/null && Src_post || zzwarn "Src_post not set."
 
 # back to ZBUILD_root
         popd
@@ -93,5 +93,5 @@ pushd $ZBUILD_root
 
 popd # backto PWD
 zprint "Zbuild Finished."
-stars
+zzstars
 # The End
